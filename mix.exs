7d924f811afae39901b78afdc01df1493ea73f5b
e.famiev <famiev91@gmail.com>
2025-03-14 16:13:34 +0500

    Add os_mon

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule LifeComplex.MixProject do
   def application do
     [
       mod: {LifeComplex.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
